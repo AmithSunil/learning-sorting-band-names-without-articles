0011a85d4df8b9eb546834f9839ba3012fbbb36a
AmithSunil <crispyfingers99@gmail.com>
2023-05-21 14:35:57 +0530

    Final

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bands = [
   "Pierce the Veil",
   "Norma Jean",
   "The Bled",
-  "Say Anything",
   "The Midway State",
+  "Say Anything",
   "We Came as Romans",
   "Counterparts",
   "Oh, Sleeper",
@@ -23,5 +23,4 @@ const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
 document.querySelector("#bands").innerHTML = sortedBands
   .map((band) => `<li>${band}</li>`)
   .join("");
-
 console.log(sortedBands);
